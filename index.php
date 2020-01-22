6212645923aba69d55836070104d7320a59df3a9
Jesus  Ocaña <jesus_aguilera_o@hotmail.com>
2020-01-21 19:00:07 -0600

    changes

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php require_once 'header.php' ?>
 <div class="page-home">
+    <img class="line-home" src="assets/img/line-home.svg" alt="">
     <section class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 text-center">
+                <small class="text-small">Desarrollador web</small>
                 <h1>Hola mi nombre es Jesus Ocaña, me dedico al desarrollo web para crear presencia en internet</h1>
             </div>
+            <div class="col-md-12 d-flex justify-content-center mt-3">
+                <a class="btn-primary" href="#!">Mi trabajo <img src="assets/img/arrow-small.svg" alt=""></a>
+            </div>
+        </div>
+    </section>
+    <section class="bg-black">
+        <div class="container mt-5">
+            <img class="banner" src="assets/img/banner.jpg" alt="">
         </div>
     </section>
 </div>
